4b8b77b806582bffa09b2a68c2be5de5f9cb3796
emrahselek <emrahselek10@gmail.com>
2021-08-08 17:45:31 -0600

    asa

--- src/testing01/emrah.java
+++ src/testing01/emrah.java
@@ -3,7 +3,8 @@ package testing01;
 public class emrah {
     public static void main(String[] args) {
 
-        System.out.println("Don`t touch my code :) ");
+        System.out.println("Don`t touch my code :)..... ");
+        System.out.println("new");
 
     }
 }
