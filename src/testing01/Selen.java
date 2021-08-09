3ec96e88af726972b3708986c3d5c2c97af2979b
esenakyol <esen.akyol.88@gmail.com>
2021-08-08 23:05:10 -0500

    no more change

--- src/testing01/Selen.java
+++ src/testing01/Selen.java
@@ -3,5 +3,6 @@ package testing01;
 public class Selen {
     public static void main(String[] args) {
         System.out.println("Selen");
+        System.out.println("This was a mistake");
     }
 }
