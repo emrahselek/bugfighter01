a7d003a63442f61bfcf56b87c4038b570f01f9ff
esenakyol <esen.akyol.88@gmail.com>
2021-08-08 22:01:56 -0500

    new version

--- src/testing01/esen.java
+++ src/testing01/esen.java
@@ -3,5 +3,6 @@ package testing01;
 public class esen {
     public static void main(String[] args) {
         System.out.println("Working on Git/GitHub");
+        System.out.println("I need to work on Java");
     }
 }
