c5b02d6a5ecfd32e185a18dac3ee0f3147f631c7
olcayayata <olcayayata4@gmail.com>
2021-08-10 10:31:00 -0400

    change

--- src/javaPractice/_Aug10.java
+++ src/javaPractice/_Aug10.java
@@ -6,8 +6,23 @@ public class _Aug10 {
   	Q1) swap two numbers without creating and using a third variable?
 		int a = 5;
 		int b = 8;
+		//code
+
+
+
 */
 		//code
+        int a = 5;
+        int b = 8;
+        System.out.println("a = " + a);//5
+        System.out.println("b = " + b);//8
+        int temp = a;
+      // a=b;
+      //  b=temp;
+        a=a+b;
+        b=a-b;
+        System.out.println ("a = " + a);
+        System.out.println("b = " + b);
 
 
 
