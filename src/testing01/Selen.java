b71fe2823f04daa1d3ee7eff4fd89089fd1d5307
selenucar <slnucartx@gmail.com>
2021-08-09 20:57:28 -0500

    m

--- src/testing01/Selen.java
+++ src/testing01/Selen.java
@@ -3,5 +3,7 @@ package testing01;
 public class Selen {
     public static void main(String[] args) {
         System.out.println("Selen");
+        // mümkünü gayrimumkun gayrimumkunu mümkün yapmak mümkün değildir
     }
+
 }
