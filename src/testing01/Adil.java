2542a4a20f95057afbc659d66374ec037a9427a3
Adil Demirleng <demirleng@gmail.com>
2021-08-10 11:06:58 -0400

    hava durumu

--- src/testing01/Adil.java
+++ src/testing01/Adil.java
@@ -3,5 +3,6 @@ package testing01;
 public class Adil {
     public static void main(String[] args) {
         System.out.println("Sol yanim agriyor anne.");
+        System.out.println("Bugun hava cok sicak.");
     }
 }
