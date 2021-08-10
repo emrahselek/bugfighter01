f2f4223f6e87557a6d8c11753ddf4c7b2e94b3ef
~!@#$%^&*()_+}{ <88084445+emrahselek@users.noreply.github.com>
2021-08-10 11:29:05 -0600

    Update Aug10New.java

--- src/javaPractice/Aug10New.java
+++ src/javaPractice/Aug10New.java
@@ -3,7 +3,7 @@ package javaPractice;
 public class Aug10New {
 
     public static void main(String[] args) {
-/*
+/**
   	Q1) swap two numbers without creating and using a third variable?
 		int a = 5;
 		int b = 8;
@@ -26,13 +26,47 @@ public class Aug10New {
 /*
 	Q2) Create 3 wrapper classes and print max and min values of the wrapper classes.
 */
-        //code
+        System.out.println("max value of Integer: "+Integer.MAX_VALUE);
+        System.out.println("max value of Byte: "+Byte.MAX_VALUE);
+        System.out.println("max value of Short: "+Short.MAX_VALUE);
+        System.out.println("min value of Integer: "+Integer.MIN_VALUE);
+        System.out.println("min value of Byte: "+Byte.MIN_VALUE);
+        System.out.println("min value of Short: "+Short.MIN_VALUE);
 
+	    
+	    
  /*
     Q3)  a user should enter start and end number. create a loop to print even numbers from start to end
  */
 
+public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter first and last number");
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        if (num1 < num2) {
 
+            for (int i = num1; i < num2; i++) {
+                if (i % 2 == 0) {
+                    System.out.print(i + " ");
+
+                }
+
+            }
+        }else{
+            for (int i = num2; i <num1; i++) {
+                if (i % 2 == 0) {
+                    System.out.print(i + " ");
+
+                }
+
+            }
+
+
+        }
+    
+
+	    
 
  /*
     Q4)  copy q3 solutions to q4, and add a new feature
@@ -50,6 +84,15 @@ public class Aug10New {
 	You have given an integer array which contains numbers from 1 to 25 but one number is missing,
 	you need to write a Java program to find that missing number in an array.
 	[1, 3, 8, 4, 5, 10, 7, 2, 9, 6, 11, 12, 14, 17, 16, 23, 18, 19, 20, 21, 22, 15, 24, 25]
+*/
+        //code
+
+	
+/*
+Q6)  from 1 to 25 but 3 numbers are missing
+
+	[1, 3, 8, 4, 5, 10, 7, 2, 9, 6, 11, 14, 17, 16, 23, 18, 19, 20, 22, 15, 24, 25]
+		use different algorithm from the previous question
 */
         //code
 
