0fc805d259a24ea3582cdf24c3641f3af49e217f
huseyinozkaya <ozkayahuseyin1@gmail.com>
2021-08-11 16:16:30 -0400

    huseyin

---
+++ src/javaPractice/Aug12.java
@@ -0,0 +1,33 @@
+package javaPractice;
+
+public class Aug12 {
+    public static void main(String[] args) {
+
+        /**
+         Q10 - ask user to enter a name,
+         print on the console how many characters are there
+
+
+         */
+
+        /**
+         registrationList {Abdrurrahman, Bilal, Hatice, Esen, Emrah, Huseyin, Olcay, Mustafa}
+            Ask user to enter a name, and check if the name is in the refistrationList
+                if so, print "You are team 2 member, brighter future is very close"
+                if not, print on the console "Contact with Team leader for registration"
+
+
+         */
+
+        /**
+         Q11 - ask user to enter a year, and check if it is leap year
+         Leap year : a year that has 29 days in a february
+
+         gııgle for condition of the leap year
+         */
+
+
+
+
+    }
+}
