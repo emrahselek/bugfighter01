efe22df0cc01612cbf4fd039cb378300e3c1208a
mcemek <thestarpets@gmail.com>
2021-08-12 21:13:15 -0400

    version03

--- src/testing01/mustafa.java
+++ src/testing01/mustafa.java
@@ -99,6 +99,26 @@ int a1 = 15;
 
 
 
+/*
+		How to find the missing number in integer array of 1 to 25?
+​
+				Note:
+				You have given an integer array which contains numbers from 1 to 25 but one number is missing,
+				you need to write a Java program to find that missing number in an array.
+				[1, 3, 8, 4, 5, 10, 7, 2, 9, 6, 11, 12, 14, 17, 16, 23, 18, 19, 20, 21, 22, 15, 24, 25]
+    	*/
 
+
+
+        int arr[] = {1, 3, 8, 4, 5, 7, 10, 2, 12, 6, 11, 14, 17, 16, 9, 23, 18, 19, 20, 22, 15, 24,25};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+
+        for(int i =1 ; i<26 ; i++) {
+            if(i!=arr[i-1]) {
+                System.out.print(i +" ");
+                break;
+            }
+        }
     }
 }
