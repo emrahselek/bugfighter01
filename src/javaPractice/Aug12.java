2a43464bd7f01e9621cae7ee87a8ec3b6de4b481
huseyinozkaya <ozkayahuseyin1@gmail.com>
2021-08-12 23:03:45 -0400

    huseyin

--- src/javaPractice/Aug12.java
+++ src/javaPractice/Aug12.java
@@ -10,21 +10,7 @@ public class Aug12 {
 
          */
 
-        /**
-         registrationList {Abdrurrahman, Bilal, Hatice, Esen, Emrah, Huseyin, Olcay, Mustafa}
-            Ask user to enter a name, and check if the name is in the refistrationList
-                if so, print "You are team 2 member, brighter future is very close"
-                if not, print on the console "Contact with Team leader for registration"
 
-
-         */
-
-        /**
-         Q11 - ask user to enter a year, and check if it is leap year
-         Leap year : a year that has 29 days in a february
-
-         gııgle for condition of the leap year
-         */
 
 
 
