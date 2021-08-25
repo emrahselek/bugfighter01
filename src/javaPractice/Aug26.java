73dde5cdb09f6f8b0706fa87ac42cb6fde19b89d
huseyinozkaya <ozkayahuseyin1@gmail.com>
2021-08-25 14:24:24 -0400

    huseyin

--- src/javaPractice/Aug26.java
+++ src/javaPractice/Aug26.java
@@ -1,12 +1,27 @@
 package javaPractice;public class Aug26 {
     public static void main(String[] args) {
         /**
-         Q-23)
+         Q-22)
 
          Show on the console one word as it is, and next word as reversed
          EXAMPLE
          String = "SQL takes time to learn, but it is useful software."
                 =SQL sekat time ot learn, tub it si useful erawtfos.
+         */
+
+
+        /**
+         Q-23)
+         String = "Java and Python are Programming languages. I use java for practices. What can I do sometimes with Java."
+         write a program to count "java"
+         */
+
+
+
+        /**
+         Q-24)
+         String = "Java and Python are Programming languages. I use java for practices. What can I do sometimes with Java."
+         How many times each word is used in the String.
          */
     }
 }
