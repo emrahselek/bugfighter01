7778e886fe44196e84b7d614123e3ed7ed02989f
huseyinozkaya <ozkayahuseyin1@gmail.com>
2021-08-26 17:10:19 -0400

    huseyin

--- src/javaPractice/RandomNameGenerator.java
+++ src/javaPractice/RandomNameGenerator.java
@@ -2,6 +2,10 @@ package javaPractice;
 
 public class RandomNameGenerator {
     public static void main(String[] args) {
-        
+        /**
+         Ask user to enter names one by one
+         Create a program that picks name randomly
+         */
+
     }
 }
