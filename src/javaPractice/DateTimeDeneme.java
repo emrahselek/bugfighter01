43f80317f43b7f6a4e18169ce0181608c24feae7
huseyinozkaya <ozkayahuseyin1@gmail.com>
2021-09-08 12:33:22 -0400

    huseyin

--- src/javaPractice/DateTimeDeneme.java
+++ src/javaPractice/DateTimeDeneme.java
@@ -1,22 +1,27 @@
 package javaPractice;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.Period;
 import java.util.Date;
 import java.util.Scanner;
 import java.util.logging.SimpleFormatter;
 
 public class DateTimeDeneme {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         LocalDate currentDate1 = LocalDate.now();
         System.out.println(currentDate1);
         Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter Date of Your Birthday Day/Month/Year");
         String DOB = scan.next();
         System.out.println(DOB);
 
-        Date dateDOB = new SimpleDateFormat("yyyy/MM/dd").parse(DOB);
 
 
-
+        Period age = Period.between(LocalDate.parse(DOB),currentDate1);
+        System.out.println(age.getYears());
     }
 }
+
