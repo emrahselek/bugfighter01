6fc5a04aa91f4a4054b13d00a50a37df2ceae111
huseyinozkaya <ozkayahuseyin1@gmail.com>
2021-09-08 14:07:39 -0400

    huseyin

--- src/javaPractice/Sep08DateTime.java
+++ src/javaPractice/Sep08DateTime.java
@@ -1,12 +1,30 @@
 package javaPractice;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Period;
+import java.util.Scanner;
+
 public class Sep08DateTime {
     public static void main(String[] args) {
-        /**
-         * Q1. Use notepad the code this question
-         * get a string from user and check if the string is palindrome
-         */
 
+        LocalDate currentDate = LocalDate.now();
+        LocalTime currentTime = LocalTime.now();
+        System.out.println(currentDate);
+        System.out.println(currentTime);
+        int year = currentDate.getYear();
+        System.out.println(year);
+     //   System.out.println(currentDate.getHour());
+        System.out.println(currentDate.getDayOfWeek());
+
+        LocalDate dateOfBirth = LocalDate.of(1980,06,25);
+        System.out.println(dateOfBirth);
+        System.out.println(dateOfBirth.getDayOfWeek());
+
+        Period age = Period.between(currentDate,dateOfBirth);
+        System.out.println(Math.abs(age.getYears()));
+        System.out.println(currentDate.isAfter(dateOfBirth));
 
         /**
          * Create a current date and time
@@ -27,5 +45,13 @@ public class Sep08DateTime {
          *
          *     Print on the console current date for that time zone and name of the country is selected
          */
+
+
     }
+
+    public static void panda(){
+
+    }
+
+
 }
